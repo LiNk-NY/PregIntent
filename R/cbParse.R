8f5c989cf098dead8f9f9dbfca17419a02f9e0c9
LiNk-NY <mramos09@gmail.com>
2017-10-09 19:57:50 -0400

    annotate code

--- R/cbParse.R
+++ R/cbParse.R
@@ -1,19 +1,28 @@
 # Read text version of codebook
 
 surv <- readLines("docs/Final_Pregnancy_Intentions_Survey.txt")
+
+# Get index of lines with question numbers
 starts <- grep("^Q[0-9]", surv, value = FALSE)
 ends <- starts[-1]-1
 starts <- starts[-length(starts)]
+
+# Create ranges for valid chunks
 ranges <- cbind(starts, ends)
+
+# Read lines as chunks, one chunk per range/question
 readChunks <- apply(ranges, 1L, function(x) {
     surv[seq(x[[1]], x[[2]])]
 })
 
+# Find chunks with close ended response values
 withValues <- vapply(readChunks, function(x)
     any(grepl("\\([0-9]{1,2}\\)", x)), logical(1L))
 
+# Take only chunks with response values
 readChunks <- readChunks[withValues]
 
+# Remove lines in chunks that are not responses
 cleanChunks <- lapply(readChunks, function(x) {
     goodLines <- grep("^Q[0-9]|\\([0-9]{1,2}\\)", x, value = TRUE)
     grep("^Skip", goodLines, invert = TRUE, value = TRUE)
