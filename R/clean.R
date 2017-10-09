40e09c375926cd82868ab7a66819d313c05b74d7
LiNk-NY <mramos09@gmail.com>
2017-10-09 19:54:45 -0400

    clean read-in of data, assign variable names

--- R/clean.R
+++ R/clean.R
@@ -1,11 +1,23 @@
 library(readxl)
 
-vars <- read_excel("../data/surveyresults_2099.xlsx", n_max = 1)
+vars <- read_excel("data/surveyresults_2099.xlsx", n_max = 1)
 
-namesVec <- unlist(vars[1, ])
+namesVect <- unlist(vars[1, ])
+
+vIdx <- grep("^V[0-9]", names(namesVect))
+names(namesVect)[vIdx] <- namesVect[vIdx]
 
 # Varnames and questions
-codebook <- cbind(varname = names(namesVec), question = namesVec)
+codebook <- cbind(varname = names(namesVect), question = namesVect)
 
-pregint <- read_excel("../data/surveyresults_2099.xlsx", skip = 2, col_names = FALSE)
+pregint <- read_excel("data/surveyresults_2099.xlsx", skip = 2,
+    col_names = names(namesVect))
 
+varsToRM <- c("ResponseSet", "Name", "ExternalDataReference", "EmailAddress",
+    "IPAddress", "Status", "opp", "RISN", "V")
+
+# Remove a few variables that have none or one response value
+pregint <- pregint[, -which(names(pregint) %in% varsToRM)]
+
+# Take only GC = 1 responses
+pregint <- pregint[pregint$gc == 1L, ]
