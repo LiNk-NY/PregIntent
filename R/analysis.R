7c92621e6237cfbf339949e996bf41d0b5c93f83
LiNk-NY <mramos09@gmail.com>
2017-10-17 01:07:13 -0400

    code outcome variable

--- R/analysis.R
+++ R/analysis.R
@@ -5,6 +5,17 @@ gender <- .recodeFactors(pregint, splitFrames$Q1.2)
 table(gender)
 all.equal(sum(table(gender)), 2099)
 
+## Main outcome
+codebook[rownames(codebook) == "Q3.34", ]
+codebook[rownames(codebook) == "Q121", ]
+
+pregFeel <- vector("character", 2099)
+## "Ultimately, how would you feel about being pregnant right now?"
+pregFeel[gender == "female"] <- .recodeFactors(pregint, splitFrames$Q3.34)[gender == "female"]
+pregFeel[gender == "male"] <- .recodeFactors(pregint, splitFrames$Q121)[gender == "male"]
+table(pregFeel)
+
+## State of residence
 residence <- .recodeFactors(pregint, splitFrames$Q110)
 table(residence)
 sum(table(residence))
