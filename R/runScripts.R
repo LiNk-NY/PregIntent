ab0d3280174b453d447c90f2edee0d726dcd59fb
LiNk-NY <mramos09@gmail.com>
2017-10-20 11:05:50 -0400

    merge env vars with dataset

--- R/runScripts.R
+++ R/runScripts.R
@@ -7,5 +7,31 @@ filesToSource <- filesToSource[fileOrd]
 ## Source all scripts
 invisible(lapply(file.path("R", filesToSource), source))
 
-## remove variables
-rm(filesInDir, filesToSource, fileOrd)
+## remove variables (if not already removed)
+# rm(filesInDir, filesToSource, fileOrd)
+
+
+variables <- ls()[!ls() %in% c("codebook", "recodeFactors", "splitFrames")]
+names(variables) <- variables
+dfsLog <- vapply(variables, function(x) is.data.frame(get(x)), logical(1L))
+
+
+DFs <- do.call(cbind,
+        lapply(variables[dfsLog], function(x) get(x))
+)
+## Remove vars that already were recoded
+pregint <- pregint[, -which(names(pregint) %in% grep("^Q", names(DFs), value = TRUE))]
+
+DFs <- do.call(cbind,
+        lapply(variables[dfsLog], function(x) get(x))
+)
+
+DFvecs <- do.call(cbind.data.frame, lapply(variables[!dfsLog], get))
+
+pregRec <- cbind(DFs, DFvecs)
+
+names(pregRec) <- gsub("pregint\\.", "", names(pregRec))
+anyDuplicated(gsub("pregint\\.", "", names(pregRec)))
+
+## Save Dataset
+readr::write_csv(pregRec, "data/pregint.csv")
