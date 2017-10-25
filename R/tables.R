3043f570fb735d37f9262690478f2558a2976766
LiNk-NY <mramos09@gmail.com>
2017-10-25 00:35:52 -0400

    add results

--- R/tables.R
+++ R/tables.R
@@ -4,7 +4,7 @@
     vec <- round(c(n = sum(!is.na(var)),
     mean = mean(var, na.rm = na.rm),
     stddev = sd(var, na.rm = na.rm)), 2)
-    matrix(vec, nrow = 1, dimnames = list(as.character(substitute(var)), names(vec)))
+    matrix(vec, nrow = 1, dimnames = list(objName, names(vec)))
 }
 
 .prop <- function(var) {
@@ -16,6 +16,18 @@
         dimnames = list(names(table(var)), "perc"))
     cbind.data.frame(counts, props)
 }
+
+.crossTab <- function(var1, var2) {
+    if (is.data.frame(var1))
+        var1 <- var1[[1L]]
+    if (is.data.frame(var2))
+        var2 <- var2[[1L]]
+    counts <- table(var1, var2)
+    props <- prop.table(table(var1, var2), 1L)
+    crossnames <- dimnames(counts)
+    vals <- paste0(counts, " (", round(props, 2), ")")
+    matrix(vals, ncol = 2, dimnames = crossnames)
+}
 ## Bivariable Table 1
 rbind(.meansd(age),
 .meansd(childnum)
@@ -26,5 +38,15 @@ rbind(.prop(gender),
 .prop(hispanic),
 .prop(educ),
 .prop(idealCrit),
-.prop(avoidPreg)
+.prop(avoidPreg),
+.prop(pregControl)
+)
+
+rbind(.crossTab(gender, pregFeel),
+      .crossTab(regionOrg, pregFeel),
+      .crossTab(hispanic, pregFeel),
+      .crossTab(educ, pregFeel),
+      .crossTab(idealCrit, pregFeel),
+      .crossTab(avoidPreg, pregFeel),
+      .crossTab(pregControl, pregFeel)
 )
