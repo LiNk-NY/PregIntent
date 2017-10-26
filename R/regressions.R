9caed03a680b3ebed1138d710f0e3838c447eed0
LiNk-NY <mramos09@gmail.com>
2017-10-26 13:53:26 -0400

    regression table

--- R/regressions.R
+++ R/regressions.R
@@ -1,6 +1,19 @@
+## Results
+library(broom)
+
 ## Models
 modelDF <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
     idealCrit, avoidPreg, pregPlan, pregFeel)
+
 names(modelDF) <- c("age", "childnum", "gender", "hispanic", "idealCrit",
     "avoidPreg", "pregPlan", "pregFeel")
-glm(pregFeel ~ ., data = modelDF, family = "binomial")
+
+pregFeel <- factor(pregFeel)
+contrasts(pregFeel)
+
+t1fit <- glm(pregFeel ~ ., data = modelDF, family = "binomial")
+
+
+restab <- tidy(t1fit) %>% mutate(estimate = exp(estimate)) %>%
+    cbind.data.frame(as.data.frame(exp(confint(t1fit))), row.names = NULL) %>%
+    select(-std.error, -statistic)
