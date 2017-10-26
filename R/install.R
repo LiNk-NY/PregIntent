ff3aac973fac581404c1c4f8c4e52258df273e15
LiNk-NY <mramos09@gmail.com>
2017-10-25 23:54:57 -0400

    add a couple of pkgs

--- R/install.R
+++ R/install.R
@@ -1,5 +1,6 @@
 ## Script to easily install packages
-packages <- c("dplyr", "tidyr", "readxl", "maps", "Hmisc", "plyr", "reshape2")
+packages <- c("dplyr", "tidyr", "readxl", "maps", "Hmisc", "plyr", "reshape2",
+              "forcats", "magrittr")
 
 for (pkg in packages) {
     if (!requireNamespace(pkg))
