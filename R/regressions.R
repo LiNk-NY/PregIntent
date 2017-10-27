2336b0c005f0aa930c1fb2a795932d7af3656662
LiNk-NY <mramos09@gmail.com>
2017-10-26 23:48:34 -0400

    add regression tables

--- R/regressions.R
+++ R/regressions.R
@@ -1,6 +1,43 @@
+## Load pkg
+library(broom)
+
 ## Models
-modelDF <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
+modeldf <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
     idealCrit, avoidPreg, pregPlan, pregFeel)
-names(modelDF) <- c("age", "childnum", "gender", "hispanic", "idealCrit",
+
+names(modeldf) <- c("age", "childnum", "gender", "hispanic", "idealCrit",
     "avoidPreg", "pregPlan", "pregFeel")
-glm(pregFeel ~ ., data = modelDF, family = "binomial")
+
+fit1 <- glm(pregFeel ~ ., data = modeldf, family = "binomial")
+
+tidy(fit1) %>%
+    mutate(estimate = exp(estimate)) %>%
+    cbind.data.frame(exp(confint(fit1)), row.names = NULL) %>%
+    select(-statistic, -std.error) %>% select(-p.value, everything())
+
+## Models
+modeldf <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
+    idealCrit, avoidPreg, pregPlan, avoidControl)
+
+names(modeldf) <- c("age", "childnum", "gender", "hispanic", "idealCrit",
+    "avoidPreg", "pregPlan", "avoidControl")
+
+fit2 <- glm(avoidControl ~ ., data = modeldf, family = "binomial")
+
+tidy(fit2) %>%
+    mutate(estimate = exp(estimate)) %>%
+    cbind.data.frame(exp(confint(fit2)), row.names = NULL) %>%
+    select(-statistic, -std.error) %>% select(-p.value, everything())
+
+modeldf <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
+    idealCrit, avoidPreg, pregPlan, becomeControl)
+
+names(modeldf) <- c("age", "childnum", "gender", "hispanic", "idealCrit",
+    "avoidPreg", "pregPlan", "becomeControl")
+
+fit3 <- glm(becomeControl ~ ., data = modeldf, family = "binomial")
+
+tidy(fit3) %>%
+    mutate(estimate = exp(estimate)) %>%
+    cbind.data.frame(exp(confint(fit3)), row.names = NULL) %>%
+    select(-statistic, -std.error) %>% select(-p.value, everything())
