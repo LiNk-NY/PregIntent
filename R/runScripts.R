05636470dfd109903927b46586b5a163e1f2ee8a
LiNk-NY <mramos09@gmail.com>
2017-10-29 13:45:34 -0400

    fix script

--- R/runScripts.R
+++ R/runScripts.R
@@ -15,25 +15,26 @@ invisible(lapply(filePaths, source))
 variables <- ls()[!ls() %in% c("codebook", "recodeFactors",
     "splitFrames", "adjustVarVal")]
 names(variables) <- variables
-dfsLog <- vapply(variables, function(x) is.data.frame(get(x)), logical(1L))
+dfsLog <- vapply(variables, function(x) is.data.frame(get(x)) && x != "pregint", logical(1L))
+pullNames <- variables[dfsLog]
 
-
-DFs <- do.call(cbind,
-        lapply(variables[dfsLog], function(x) get(x))
+DFs <- do.call(cbind.data.frame,
+        lapply(variables[dfsLog], function(x) {
+            ax <- get(x)
+            if (length(names(ax)) == 1L)
+                names(ax) <- x
+            else {
+                names(ax)
+            }
+            ax
+        })
 )
-## Remove vars that already were recoded
-pregint <- pregint[, -which(names(pregint) %in% grep("^Q", names(DFs), value = TRUE))]
 
-DFs <- do.call(cbind,
-        lapply(variables[dfsLog], function(x) get(x))
-)
-
-DFvecs <- do.call(cbind.data.frame, lapply(variables[!dfsLog], get))
+DFvecs <- do.call(cbind.data.frame,
+    lapply(variables[!dfsLog][-which(variables[!dfsLog] == "pregint")], get))
 
 pregRec <- cbind(DFs, DFvecs)
 
-names(pregRec) <- gsub("pregint\\.", "", names(pregRec))
-anyDuplicated(gsub("pregint\\.", "", names(pregRec)))
-
+pregRec <- cbind.data.frame(pregint, pregRec)
 ## Save Dataset
 readr::write_csv(pregRec, "data/pregint.csv")
