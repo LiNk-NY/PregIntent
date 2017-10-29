20a5ffd2c73fd5c185810fbde070c617a9e3ad7b
LiNk-NY <mramos09@gmail.com>
2017-10-29 13:44:44 -0400

    update regression script for subset

--- R/regressions.R
+++ R/regressions.R
@@ -1,23 +1,44 @@
 ## Load pkg
 library(broom)
+library(dplyr)
+library(tidyr)
+
+# Subset model ------------------------------------------------------------
+
+pregint <- read.csv("data/pregint.csv")
+subGroup <- !is.na(pregint$pregFeel)
+subdata <- pregint[subGroup, ]
+rm(pregint)
+
+attach(subdata)
+source("R/relevel.R")
 
 ## Models
 modeldf <- cbind.data.frame(age, as.numeric(childnum), gender, hispanic,
-    idealCrit, avoidPreg, pregPlan, pregFeel)
+    idealCrit, avoidPreg, pregPlan, pregFeel, currentSit, race, relationship,
+    incCat)
 
 names(modeldf) <- c("Age", "childnum", "Gender", "Hispanic", "idealCrit",
-    "avoidPreg", "pregPlan", "pregFeel")
+    "avoidPreg", "pregPlan", "pregFeel", "currentSit", "Race", "Relationship",
+    "IncomeCat")
 
 fit1 <- glm(pregFeel ~ ., data = modeldf, family = "binomial")
 
 (posneg <- tidy(fit1) %>%
     mutate(estimate = round(exp(estimate), 2)) %>%
     cbind.data.frame(round(exp(confint(fit1)), 1), row.names = NULL) %>%
+    dplyr::rename(lowCI = `2.5 %`, upCI = `97.5 %`) %>%
     select(-statistic, -std.error) %>% select(-p.value, everything()) %>%
     mutate(p.value = format.pval(pv = p.value, digits = 3, eps = 0.001)) %>%
-    unite("95% CI", c("2.5 %", "97.5 %"), sep = " - ") %>%
-        rename(beta = "estimate", variable = "term")
+    unite("95% CI", c("lowCI", "upCI"), sep = " - ") %>%
+        dplyr::rename(beta = "estimate", variable = "term")
 )
+
+posneg
+write.csv(posneg, "data/regFeel.csv")
+
+# Models using full data --------------------------------------------------
+
 
 ## Models
 modeldf <- cbind.data.frame(age, as.numeric(childnum), gender, educ,
@@ -32,7 +53,7 @@ fit2 <- glm(avoidControl ~ ., data = modeldf, family = "binomial")
     mutate(estimate = round(exp(estimate), 2)) %>%
     cbind.data.frame(round(exp(confint(fit2)), 1), row.names = NULL) %>%
     select(-statistic, -std.error) %>% select(-p.value, everything()) %>%
-    mutate(p.value = format.pval(pv = p.value, digits = 3, eps = 0.001)) %>%
+    mutate(p.value = format.pval(pv = p.value, digits = 2, eps = 0.001)) %>%
     unite("95% CI", c("2.5 %", "97.5 %"), sep = " - ") %>%
         rename(beta = "estimate", variable = "term")
 )
@@ -54,8 +75,6 @@ fit3 <- glm(becomeControl ~ ., data = modeldf, family = "binomial")
         rename(beta = "estimate", variable = "term")
 )
 
-posneg
-write.csv(posneg, "data/regFeel.csv")
 
 avoid
 write.csv(avoid, "data/regAvoid.csv")
