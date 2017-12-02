475e984bf8692b7a1bbdb3c91f73f0127c20d7a5
LiNk-NY <mramos09@gmail.com>
2017-12-02 02:18:29 -0500

    add digits arg

--- R/table-helpers.R
+++ R/table-helpers.R
@@ -1,39 +1,39 @@
 ## Helper functions for constructing results
-.meansd <- function(numVar, na.rm = TRUE, varName = NULL) {
+.meansd <- function(numVar, na.rm = TRUE, varName = NULL, digits = 2) {
     if (is.null(varName)) {
     varName <- as.character(substitute(numVar))
     varName <- varName[[length(varName)]]
     }
     stopifnot(S4Vectors::isSingleString(varName))
     numVar <- as.numeric(numVar)
-    m <- round(mean(numVar, na.rm = na.rm), 2)
-    stddev <- round(sd(numVar, na.rm = na.rm), 2)
+    m <- round(mean(numVar, na.rm = na.rm), digits)
+    stddev <- round(sd(numVar, na.rm = na.rm), digits)
     matrix(paste0(m, " (", stddev, ")"), ncol = 1L,
         dimnames = list(varName, "M (SD)"))
 }
 
-.prop <- function(numVar) {
+.prop <- function(numVar, digits = 1) {
     if (is.data.frame(numVar))
         numVar <- numVar[[1L]]
     counts <- table(numVar)
-    props <- round(prop.table(table(numVar))*100, 1)
-    vals <- paste0(counts, " (", round(props, 2), ")")
+    props <- round(prop.table(table(numVar))*100, digits)
+    vals <- paste0(counts, " (", props, ")")
     matrix(vals, ncol = 1, dimnames = list(names(table(numVar)), "n (%)"))
 }
 
-.crossTab <- function(var1, var2) {
+.crossTab <- function(var1, var2, digits = 2) {
     if (is.data.frame(var1))
         var1 <- var1[[1L]]
     if (is.data.frame(var2))
         var2 <- var2[[1L]]
     counts <- table(var1, var2)
-    props <- prop.table(table(var1, var2), 1L)
+    props <- round(prop.table(table(var1, var2), 1L)*100, digits)
     crossnames <- dimnames(counts)
-    vals <- paste0(counts, " (", round(props, 2), ")")
+    vals <- paste0(counts, " (", props, ")")
     matrix(vals, ncol = 2, dimnames = crossnames)
 }
 
-.groupMeans <- function(numVar, outcome) {
+.groupMeans <- function(numVar, outcome, digits = 2) {
     varName <- as.character(substitute(numVar))
     varName <- varName[[length(varName)]]
     stopifnot(S4Vectors::isSingleString(varName))
@@ -44,8 +44,8 @@
     splitSet <- splitSet[order(na.omit(unique(outcome)))]
     groupNames <- names(splitSet)
     res <- vapply(seq_along(splitSet), function(i, x) {
-        m <- round(mean(x[[i]], na.rm = TRUE), 2)
-        std <- round(sd(x[[i]], na.rm = TRUE), 2)
+        m <- round(mean(x[[i]], na.rm = TRUE), digits)
+        std <- round(sd(x[[i]], na.rm = TRUE), digits)
         paste0(m, " (", std, ")")
     }, character(1L), x = splitSet)
     resMat <- matrix(res, nrow = 1L, dimnames = list(varName, groupNames))
@@ -81,7 +81,9 @@
         rep("", lvls-1)), ncol = 1L, dimnames = list(labels, "p.value"))
 }
 
-.comparisonTable <- function(..., outcome, headerRow = NULL, outcomeOrder = NULL, deparse.level = 2) {
+.comparisonTable <- function(..., outcome, headerRow = NULL,
+    outcomeOrder = NULL, deparse.level = 2, digits = 2)
+{
     listvars <- as.list(substitute(list(...)))[-1L]
     nams <- vapply(listvars, function(x) {
         switch(deparse.level + 1L,
@@ -93,9 +95,11 @@
     numerics <- vapply(args, is.numeric, logical(1L))
     lapply(seq_along(args), function(i, x, compVar) {
         if (is.numeric(x[[i]])) {
-            cbind(.meansd(x[[i]], varName = names(x[i])), .groupMeans(x[[i]], compVar))
+            cbind(.meansd(x[[i]], varName = names(x[i]), digits = digits),
+                .groupMeans(x[[i]], compVar, digits = digits))
         } else {
-            cbind(.prop(x[[i]]), .crossTab(x[[i]], compVar))
+            cbind(.prop(x[[i]], digits = digits),
+                .crossTab(x[[i]], compVar, digits = digits))
         }
     }, compVar = outcome, x = args)
 }
