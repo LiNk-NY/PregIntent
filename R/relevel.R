c6e7f69c611b063f2690332f69448101fad8d5d9
LiNk-NY <mramos09@gmail.com>
2018-01-07 20:15:15 -0500

    update relevel script

--- R/relevel.R
+++ R/relevel.R
@@ -2,19 +2,19 @@
 
 # recoding and releveling -------------------------------------------------
 
-idealCrit <- factor(idealCrit)
+# idealCrit <- factor(idealCrit)
 idealCrit <- relevel(idealCrit, ref = "no")
 
 avoidPreg <- relevel(avoidPreg, ref = "No")
-pregPlan <- factor(pregPlan)
-pregFeel <- factor(pregFeel)
+# pregPlan <- factor(pregPlan)
+# pregFeel <- factor(pregFeel)
 
 educLev <- c("LT/some HS", "HS diploma/GED", "Some college",
     "College degree/Some Grad", "Grad degree")
-levels(educ[[1L]]) <- educLev
-educ <- factor(educ[[1L]], ordered = TRUE)
+levels(educ) <- educLev
+educ <- factor(educ, ordered = TRUE)
 
-currentSit <- factor(currentSit)
+# currentSit <- factor(currentSit)
 currentSit <- relevel(currentSit, "don't want you/partner to become pregnant soon")
 
 race <- relevel(race, ref = "White")
