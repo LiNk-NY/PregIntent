518ed2e6f19010d2310508fa17d99b22760765ce
LiNk-NY <mramos09@gmail.com>
2018-01-24 22:56:00 -0500

    exclude codebookSheet

--- R/runScripts.R
+++ R/runScripts.R
@@ -21,7 +21,7 @@ invisible(lapply(filePaths, source))
 
 ## Remove objs that were used for cleaning
 variables <- ls()[!ls() %in% c("codebooktext", "recodeFactors",
-    "codebook", "adjustVarVal", "cleanChunks")]
+    "codebook", "adjustVarVal", "cleanChunks", "codebookSheet")]
 names(variables) <- variables
 dfsLog <- vapply(variables, function(x) is.data.frame(get(x)) && x != "pregint", logical(1L))
 pullNames <- variables[dfsLog]
