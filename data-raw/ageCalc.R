bdcd6849f2a05dbbc27bc332ec1a5b776e52bf38
LiNk-NY <mramos09@gmail.com>
2018-01-25 15:37:29 -0500

    calc m sd of age

---
+++ data-raw/ageCalc.R
@@ -0,0 +1,16 @@
+# Script to recreate Age vector from counts
+library(readxl)
+
+page <- read_excel("../data/PCP-age.xlsx")
+page <- page[seq_len(nrow(page)-1), ]
+page$COUNT <- as.integer(page[["COUNT"]])
+page$Age <- as.integer(page[["Age"]])
+
+AgeV <- apply(page, 1L, function(row) {
+    rep(row[[1]], times = row[[2]])
+})
+
+AgeV <- unlist(AgeV)
+
+mean(AgeV)
+sd(AgeV)
