3f1d151bd7644833c3383b1739fcb731c4ce275e
LiNk-NY <mramos09@gmail.com>
2018-02-18 14:39:07 -0500

    update relveling script

--- R/relevel.R
+++ R/relevel.R
@@ -1,21 +1,27 @@
-## Relevel variables
-
 # recoding and releveling -------------------------------------------------
 
-# idealCrit <- factor(idealCrit)
-idealCrit <- relevel(idealCrit, ref = "no")
+## Recode NA to not selected
+avoidPregs <- sort(grep("Q2\\.2.*\\.\\.*", names(pregint), value = TRUE))
 
-avoidPreg <- relevel(avoidPreg, ref = "No")
-# pregPlan <- factor(pregPlan)
-# pregFeel <- factor(pregFeel)
+pregint[, avoidPregs] <- lapply(avoidPregs, function(varname) {
+    actvar <- pregint[, varname, drop = TRUE]
+    levels(actvar) <- c(levels(actvar), "Not selected")
+    actvar[is.na(actvar)] <- "Not selected"
+    actvar <- relevel(actvar, ref = "Not selected")
+    actvar
+})
 
-educLev <- c("LT/some HS", "HS diploma/GED", "Some college",
-    "College degree/Some Grad", "Grad degree")
-levels(educ) <- educLev
-educ <- factor(educ, ordered = TRUE)
+## Relevel variables
+pregint$idealCrit <- relevel(pregint$idealCrit, ref = "no")
+pregint$avoidPreg <- relevel(pregint$avoidPreg, ref = "No")
 
-# currentSit <- factor(currentSit)
-currentSit <- relevel(currentSit, "don't want you/partner to become pregnant soon")
+pregint$currentSit <- relevel(pregint$currentSit,
+    "don't want you/partner to become pregnant soon")
 
-race <- relevel(race, ref = "White")
-relationship <- relevel(relationship, ref = "single")
+pregint$race <- relevel(pregint$race, ref = "White")
+pregint$relationship <- relevel(pregint$relationship, ref = "single")
+pregint$incCat <- relevel(pregint$incCat, ref = "less than $20,000")
+pregint$Q2.12_1..Q2.13_1 <- relevel(pregint$Q2.12_1..Q2.13_1, ref = "no control")
+pregint$Q3.12_1..Q3.13_1 <- relevel(pregint$Q3.12_1..Q3.13_1, ref = "no control")
+pregint$educ <- relevel(pregint$educ, ref = "HS diploma/GED")
+pregint$sex <- relevel(pregint$sex, ref = "male")
