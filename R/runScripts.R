0c8c934c3eec41b52a20789a4e3c90c5f1fd098b
LiNk-NY <mramos09@gmail.com>
2018-02-24 23:28:43 -0500

    simplify by rm merge of env objects

--- R/runScripts.R
+++ R/runScripts.R
@@ -3,11 +3,8 @@ filesToSource <- c(
     "install.R",
     "helpers.R",
     "clean.R",
-    "codebookParse.R"
-    # )
-    ,
-    "recode.R"
-    )
+    "codebookParse.R",
+    "recode.R")
 filePaths <- file.path("R", filesToSource)
 
 ## Make sure all files exist
@@ -19,35 +16,10 @@ invisible(lapply(filePaths, source))
 ## remove variables (if not already removed)
 ## rm(filesToSource, filePaths)
 
-## Remove objs that were used for cleaning
-variables <- ls()[!ls() %in% c("codebooktext", "recodeFactors",
-    "codebook", "adjustVarVal", "cleanChunks", "codebookSheet")]
-names(variables) <- variables
-dfsLog <- vapply(variables, function(x) is.data.frame(get(x)) && x != "pregint", logical(1L))
-pullNames <- variables[dfsLog]
+pregint <- pregint[, !names(pregint) %in%
+    intersect(names(recodedData), names(pregint))]
 
-DFs <- do.call(cbind.data.frame,
-        lapply(variables[dfsLog], function(x) {
-            ax <- get(x)
-            if (length(names(ax)) == 1L)
-                names(ax) <- x
-            else {
-                names(ax)
-            }
-            ax
-        })
-)
-
-DFvecs <- do.call(cbind.data.frame,
-    lapply(variables[!dfsLog][-which(variables[!dfsLog] == "pregint")], get))
-
-pregRec <- cbind(DFs, DFvecs)
-
-names(pregRec) <- gsub("recodedData\\.|feelings\\.", "", names(pregRec))
-
-pregint <- pregint[, !names(pregint) %in% intersect(names(pregRec), names(pregint))]
-
-pregData <- cbind.data.frame(pregint, pregRec)
+pregData <- cbind.data.frame(pregint, recodedData)
 
 ## Save Dataset
 # readr::write_csv(pregData, "data/pregint.csv")
